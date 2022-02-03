db705ff5a49e393a5e13a93834ca6a0eca155e0d
ilesthg <vhgm@hotmail.com>
2022-02-03 10:03:55 -0600

    ModuloDescription

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import Description from './Description';
+// ENTRE LLAVES SE EVALUA LO QUE LE PASES YA SEAN VATIABELS
+//SALTO DE LINEA CON <BR \>
+// SACAS EL TIMESTAMP VALUE {+new Date()}
+//LAS FUNCIONES QUE CREAN COMPONENTES SON MAS USADAS
+
+//Crear un componente 
+function Mensaje (){
+ return(
+  <h1>creacion de compoente</h1>
+
+ );
+}
+
 
 function App() {
+const a = 5
+const b= 7
+const mensaje = "Hola mundo"
   return (
     <div className="App">
-     Hola Mundo
+   <h1> TITULO DE LA App </h1>
+<strong>TRABAJADO EN ELLO </strong>
+
+      <Mensaje />
+      <Description color = "blue " mensaje ="Trabajando viejillo"></Description>
+      <Description color = "black " mensaje =" viejillo"></Description>
+
+<p> El resultado es :</p>
+      {a+b}
+    <br />
+      {mensaje + "  " + 'SISTEMA JSX'}
+     
+   
     </div>
   );
 }
---
+++ src/Description.js
@@ -0,0 +1,11 @@
+
+// REUTILIZAR UN COMONENTE PROPS OSN LAS PARAMETROS S
+function Description (props){
+    return(
+   <h1 style={{ color : props.color}}>{props.mensaje} </h1> 
+   
+    );
+   
+   
+   }
+   export default Description 
