001591647849789af876151bd934e2db1760653e
VICTOR HUGO GONZALEZ MONTAÑO <16171358@itculiacan.edu.mx>
2022-02-03 12:05:42 -0700

    Ok

--- courseinfo/src/Content.js
+++ courseinfo/src/Content.js
@@ -6,11 +6,18 @@ function Content(props ){
     return (
         <div>
             {props.parte1}
+            
             {props.exercises1}
-            {props.parte2}
+    
+           
+            
+            {props.parte2} 
+            
             {props.exercises2}
+        
             {props.parte3}
             {props.exercises3}
+            
             
             </div>
 
